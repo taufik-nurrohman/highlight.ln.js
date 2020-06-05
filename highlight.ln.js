2c05060c472b37211e1b881003a9ad484a814ae2
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-06-05 19:47:26 +0700

    Resolve #1

--- highlight.ln.js
+++ highlight.ln.js
@@ -1,19 +1,34 @@
 (function(win, doc, hljs) {
+    function getColorParts(el) {
+        let color = win.getComputedStyle(el).color || "", c;
+        // <https://www.regular-expressions.out/numericranges.html>
+        if (c = color.match(/^rgba\s*\(\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*[, ]\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*[, ]\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*[, ]\s*([01]|0?\.\d+)\s*\)$/i)) {
+            return [+c[1], +c[2], +c[3], +c[4]];
+        }
+        if (c = color.match(/^rgb\s*\(\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*[, ]\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*[, ]\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\s*\)$/i)) {
+            return [+c[1], +c[2], +c[3], 1];
+        }
+        return [0, 0, 0, 1];
+    }
     hljs.addPlugin({
         'after:highlightBlock': ({block}) => {
-            let lines = doc.createElement('code'),
-                blockParent = block.parentNode,
-                blockHasParent = blockParent && 'pre' === blockParent.nodeName.toLowerCase();
+            let blockParent = block.parentNode,
+                blockHasParent = blockParent && 'pre' === blockParent.nodeName.toLowerCase(),
+                lines = doc.createElement('code'),
+                numbers = [];
             if (blockHasParent) {
-                blockParent.style.display = 'flex';
-                for (let i = 0, j = block.innerHTML.replace(/\s*<br(\s[^>]*)?>\s*/g, '\n').split(/\n/).length; i < j; ++i) {
-                    lines.innerHTML += (i + 1) + '\n';
+                for (let i = 0, j = block.textContent.split(/\n/).length; i < j; ++i) {
+                    numbers.push(i + 1);
                 }
                 blockParent.insertBefore(lines, block);
-                lines.style.borderRight = '2px solid rgba(0, 0, 0, .125)';
-                block.style.flex = 1;
+                blockParent.style.display = 'flex';
+                lines.innerHTML = numbers.join('\n');
                 lines.style.textAlign = 'right';
+                lines.style.userSelect = 'none'; // Disable selection
                 lines.className = 'hljs'; // Inherit `background` and `padding` from the style sheet
+                let rgba = getColorParts(lines);
+                lines.style.borderRight = '2px solid rgba(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ',' + (rgba[3] / 10) + ')';
+                block.style.flex = 1;
             }
         }
     });
